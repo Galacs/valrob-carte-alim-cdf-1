0bc99ccfd9bbebfca8a865743b83a5a652b6a01e
Rémi Ait-Younes <remi@ait-younes.fr>
2026-02-08 12:54:33 +0100

    fw: add pins definitions

--- valrob-carte-alim-cdf-1-fw/src/main.cpp
+++ valrob-carte-alim-cdf-1-fw/src/main.cpp
@@ -20,6 +20,23 @@
 #define ILM_12V_3_PIN 5
 #define ILM_12V_4_PIN 4
 
+#define EN_5V_1_PIN 39
+#define EN_5V_2_PIN 21
+#define EN_5V_3_PIN 40
+
+#define EN_12V_1_PIN 11
+#define EN_12V_2_PIN 13
+#define EN_12V_3_PIN 8
+
+#define PG_5V_1_PIN 38
+#define PG_5V_2_PIN 47
+#define PG_5V_3_PIN 41
+
+#define PG_12V_1_PIN 10
+#define PG_12V_2_PIN 12
+#define PG_12V_3_PIN 18
+#define PG_12V_4_PIN 16
+
 float get_batt_volotage() {
   return 0.0;
 }
