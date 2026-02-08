6f2cb544a52e70bda5f6b577bdf4fc0220e1ca16
Rémi Ait-Younes <remi@ait-younes.fr>
2026-02-08 13:07:54 +0100

    fw: add power channel object

--- valrob-carte-alim-cdf-1-fw/src/main.cpp
+++ valrob-carte-alim-cdf-1-fw/src/main.cpp
@@ -37,6 +37,22 @@
 #define PG_12V_3_PIN 18
 #define PG_12V_4_PIN 16
 
+class PowerChannel {
+public:
+  PowerChannel(int pin, String channel_name, bool enabled = true,
+               float ilim = -1)
+      : m_pin(pin), m_channel_name(channel_name), m_mcu_low(!enabled),
+        m_soft_Ilim(ilim) {};
+
+  int m_pin;
+  String m_channel_name;
+  bool m_mcu_low;
+  bool m_override_low;
+  bool m_pg;
+  float m_current;
+  float m_soft_Ilim;
+};
+
 float get_batt_volotage() {
   return 0.0;
 }
@@ -47,6 +63,10 @@ void update_pwr_led() {
 
 void setup() {
   pinMode(PWR_STATUS_PIN, OUTPUT);
+  pinMode(EN_12V_2_PIN, OUTPUT);
+  digitalWrite(EN_12V_2_PIN, LOW);
+  delay(1000);
+  pinMode(EN_12V_2_PIN, INPUT);
 }
 
 void loop() {
