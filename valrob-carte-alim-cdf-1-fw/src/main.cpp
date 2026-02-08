9671d16e5502715db1fffb7a67294a7af502ba57
Rémi Ait-Younes <remi@ait-younes.fr>
2026-02-08 14:38:00 +0100

    fw: add power channel impl

--- valrob-carte-alim-cdf-1-fw/src/main.cpp
+++ valrob-carte-alim-cdf-1-fw/src/main.cpp
@@ -37,18 +37,75 @@
 #define PG_12V_3_PIN 18
 #define PG_12V_4_PIN 16
 
+enum class power_channel_state_t: uint8_t {
+  GOOD,
+  OVERRIDE,
+  EMS,
+  FAULT,
+  OFF,
+};
+
 class PowerChannel {
 public:
-  PowerChannel(int pin, String channel_name, bool enabled = true,
-               float ilim = -1)
-      : m_pin(pin), m_channel_name(channel_name), m_mcu_low(!enabled),
-        m_soft_Ilim(ilim) {};
+  PowerChannel(int en_pin, int pg_pin, int ilm_pin, String channel_name,
+               bool enabled = true, float ilim = -1)
+      : m_en_pin(en_pin), m_pg_pin(pg_pin), m_ilm_pin(ilm_pin),
+        m_channel_name(channel_name), m_mcu_low(!enabled), m_soft_Ilim(ilim) {
+    pinMode(m_en_pin, INPUT);
+    pinMode(m_pg_pin, INPUT);
+    pinMode(m_ilm_pin, INPUT);
+    if (enabled) {
+      enable();
+    }
+  };
 
-  int m_pin;
+  void trigger_EMS() {
+    m_ems = true;
+    disable();
+  };
+  void release_EMS() {
+    m_ems = false;
+    if (m_mcu_low) {
+      enable();
+    }
+  };
+
+  void disable() {
+    if (m_ems) return;
+    pinMode(m_en_pin, OUTPUT);
+    digitalWrite(m_en_pin, LOW);
+    m_mcu_low = true;
+    digitalWrite(m_en_pin, LOW);
+  };
+
+  void enable() {
+    pinMode(m_en_pin, INPUT);
+    m_mcu_low = false;
+  };
+
+  // true pg and no low en
+  power_channel_state_t get_state() {
+    if (m_mcu_low) return power_channel_state_t::OFF;
+    if (m_ems) return power_channel_state_t::EMS;
+    if (!m_mcu_low && !digitalRead(m_en_pin)) return power_channel_state_t::OVERRIDE;
+    if (!m_mcu_low && digitalRead(m_en_pin) && !digitalRead(m_pg_pin)) return power_channel_state_t::FAULT;
+    return power_channel_state_t::GOOD;
+  }
+
+  void print_state() {
+    const static String states[] = {"GOOD", "OVERRIDE", "EMS", "FAULT", "OFF"};
+    Serial.printf("%s: %s\n", m_channel_name, states[(uint8_t) get_state()]);
+  }
+
+private:
+  int m_en_pin;
+  int m_pg_pin;
+  int m_ilm_pin;
   String m_channel_name;
   bool m_mcu_low;
   bool m_override_low;
   bool m_pg;
+  bool m_ems = false;
   float m_current;
   float m_soft_Ilim;
 };
@@ -61,17 +118,27 @@ void update_pwr_led() {
 
 }
 
+PowerChannel Channel_5v_1(EN_5V_1_PIN, PG_5V_1_PIN, ILM_5V_1_PIN, "+5V 5A 1");
+
 void setup() {
-  pinMode(PWR_STATUS_PIN, OUTPUT);
-  pinMode(EN_12V_2_PIN, OUTPUT);
-  digitalWrite(EN_12V_2_PIN, LOW);
-  delay(1000);
-  pinMode(EN_12V_2_PIN, INPUT);
+  Serial.begin(115200);
+  // pinMode(PWR_STATUS_PIN, OUTPUT);
+  // pinMode(EN_12V_2_PIN, OUTPUT);
+  // digitalWrite(EN_12V_2_PIN, LOW);
+  // delay(1000);
+  // pinMode(EN_12V_2_PIN, INPUT);
 }
 
 void loop() {
-  digitalWrite(PWR_STATUS_PIN, HIGH);
-  delay(200);
-  digitalWrite(PWR_STATUS_PIN, LOW);
-  delay(200);
+  Channel_5v_1.print_state();
+  delay(500);
+  // digitalWrite(PWR_STATUS_PIN, HIGH);
+  // delay(200);
+  // digitalWrite(PWR_STATUS_PIN, LOW);
+  // delay(200);
+
+  // Channel_5v_1.disable();
+  // delay(1000);
+  // Channel_5v_1.enable();
+  // delay(1000);
 }
